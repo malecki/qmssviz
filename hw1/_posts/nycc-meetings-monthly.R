955ec0454999cd71157a4abf4a9b55029a756efc
rcquan <ryan.quan08@gmail.com>
2014-09-17 22:27:51 -0400

    used camelCase, added comments for clarification

--- hw1/_posts/nycc-meetings-monthly.R
+++ hw1/_posts/nycc-meetings-monthly.R
@@ -51,55 +51,62 @@ names(df) <- c("Name", "Date", "Time", "Location", "Topic")
 df$Date <- as.POSIXct(df$Date, format = "%m/%d/%Y")
 
 # Remove defunct committees & Committee on Finance (outliers)
-df_plot <- df %>%
-    # filter out inactive and small committees
-    filter(!str_detect(Name, ignore.case("Inactive")),
-           !str_detect(Name, ignore.case("Subcommittee")),
-           !str_detect(Name, ignore.case("Task")),
-           !str_detect(Name, ignore.case("Town"))) %>%
-    mutate(DateTime = paste(Date,Time),
-           # clean "name" variable
-           Name = str_replace(Name, "Committee on ", ""),
-           Name = str_replace(Name, ",.*$", ""),
-           Name = str_replace(Name, "and Solid Waste Management", ""),
-           Name = str_replace(Name, "Justice Services", "Justice"),
-           # create new columns for month and year with proper format
-           Month = factor(format(Date, format = "%b"), levels = month.abb),
-           Year = factor(format(Date, format = "%Y")),
-           # create new column for Status
-           Status = ifelse(Time == "Deferred", "Deferred", "Calendared"))
+dfPlot <- df %>%
+  # filter out inactive and small committees
+  filter(!str_detect(Name, ignore.case("Inactive")),
+         !str_detect(Name, ignore.case("Subcommittee")),
+         !str_detect(Name, ignore.case("Task")),
+         !str_detect(Name, ignore.case("Town"))) %>%
+  # create new variables
+  mutate(DateTime = paste(Date,Time),
+         # clean "name" variable
+         Name = str_replace(Name, "Committee on ", ""),
+         Name = str_replace(Name, ",.*$", ""),
+         Name = str_replace(Name, "and Solid Waste Management", ""),
+         Name = str_replace(Name, "Justice Services", "Justice"),
+         # create new columns for month and year with proper format
+         Month = factor(format(Date, format = "%b"), levels = month.abb),
+         Year = factor(format(Date, format = "%Y")),
+         # create new column for Status
+         Status = ifelse(Time == "Deferred", "Deferred", "Calendared")) %>%
+  # filter for meetings that were actually held
+  filter(Year %in% c(2000:2013),
+         Status == "Calendared")
 
 #############################
 # VISUALIZATION
 #############################
 
-df_plot %>%
-    filter(Year %in% c(2000:2013),
-           Status == "Calendared") %>%
-    group_by(Month) %>%
-    summarise(Count = n()) %>%
-    mutate(Proportion = (Count/sum(Count)*100)) %>%
-    
-    ggplot(aes(x = Month, y = Proportion)) +
-    geom_bar(stat = "identity") +
-    ylab("Proportion of Meetings Held (%)") +
-    scale_y_continuous(breaks = seq(0, 16, 2)) +
-    ggtitle("City Council Meetings (2000-2013)") +
-    theme_bw()
+# Total Committee Meetings Held by Month
+dfPlot %>%
+  group_by(Month) %>%
+  summarise(Count = n()) %>%
+  mutate(Proportion = (Count/sum(Count)*100)) %>%
+  
+  # bar plot
+  ggplot(aes(x = Month, y = Proportion)) +
+  geom_bar(stat = "identity") +
+  ylab("Proportion of Meetings Held (%)") +
+  # create more breaks
+  scale_y_continuous(breaks = seq(0, 16, 2)) +
+  ggtitle("City Council Meetings (2000-2013)") +
+  theme_bw()
 
+# create alphabetical vector of committee names 
+Committees <- sort(unique(dfPlot$Name))
 
-Committees <- sort(unique(df_plot$Name))
-df_plot %>%
-    # data 
-    filter(Year %in% c(2000:2013),
-           Status == "Calendared") %>%
-    xtabs(formula = ~Name + Month) %>%
-    as.data.frame() %>%
-    
-    ggvis(x = ~Month, y = ~Freq, fill = ~Name) %>%
-    filter(Name %in% eval(input_checkboxgroup(
-        choices = Committees,
-        selected = c("Aging", "Land Use"),
-        label = "Committee Name"))) %>%
-    layer_bars()
+dfPlot %>%
+  # get frequency of meetings for each committee
+  # for every month
+  xtabs(formula = ~Name + Month) %>%
+  as.data.frame() %>%
+  
+  # stacked bar plot
+  ggvis(x = ~Month, y = ~Freq, fill = ~Name) %>%
+  # interactive checkboxes for each committee
+  filter(Name %in% eval(input_checkboxgroup(
+      choices = Committees,
+      selected = c("Aging", "Land Use"),
+      label = "Committee Name"))) %>%
+  layer_bars()
   

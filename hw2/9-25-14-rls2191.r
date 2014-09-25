97738b4e99b186c8b7e7e1cc05b659c4f4f4762a
Rachel Smith <rachel.lillian.smith@gmail.com>
2014-09-25 15:36:16 -0400

    merged and sorted data

--- hw2/9-25-14-rls2191.r
+++ hw2/9-25-14-rls2191.r
@@ -7,4 +7,7 @@ str(facilities)
 dim(facilities)
 
 sample <- facilities[facilities$zone=="South-South" | facilities$zone=="Southeast" | facilities$zone=="Southwest", ]
-sample2 <- merge(sample, lgas, by="lga_id")
+sample <- merge(sample, lgas, by="lga_id")
+sample <- sample[,1:12]
+sample$total <- sample$num_doctors_fulltime  + sample$num_nurses_fulltime
+sample <- sample[order(-sample$pop_2006),]

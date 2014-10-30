e93c62215abeaf3b957f6938be1acf2e4bd90488
malecki <mike@crunch.io>
2014-10-30 19:40:08 -0400

    what we did with our first js file evar

--- labs/D3-tutorial/germs.js
+++ labs/D3-tutorial/germs.js
@@ -1,16 +1,26 @@
-window.onload = function(){
-	var hello = 'Hello world!'
-
-	d3.select('#plot-area')
-		.append('p')
-		.on('mouseover', applyHoverStyle)
-		.on('mouseout', removeHoverStyle)
-		.text(hello)
-
-	function applyHoverStyle(){
-		d3.select(this).classed({hovered: true})
+	window.onload = function(){
+		var hello = 'Hello world!'
+	
+		var plotArea = d3.select('#plot-area')
+		var mainParagraph = plotArea.append('p')
+			.on('mouseover', applyHoverStyle)
+			.on('mouseout', removeHoverStyle)
+			.text(hello)
+		var secondParagraph = plotArea.append('p')
+			.on('mouseover', applyHoverStyle)
+			.on('mouseout', removeHoverStyle)
+			.text("")
+			.classed({hovered: true})
+		var thirdParagraph = plotArea.append('p')	
+			.text("another text")
+		
+	
+	
+		function applyHoverStyle(){
+			d3.select(this).classed({hovered: true})
+		}
+		function removeHoverStyle(){
+			d3.select(this).classed({hovered : false})
+		}
+		
 	}
-	function removeHoverStyle(){
-		d3.select(this).classed({hovered : false})
-	}
-}

ea773a954253c7c337d19810e30bb01a613ec6d6
Michael Malecki <mike@crunch.io>
2014-10-30 18:01:42 -0400

    moar

--- labs/D3-tutorial/germs.js
+++ labs/D3-tutorial/germs.js
@@ -9,15 +9,21 @@ window.onload = function(){
 function draw(data){
 	var width = 700
 		,height= 600
+		,margin = {
+			left: 20,
+			right: 20,
+			top: 10,
+			bottom: 10
+		}
 		;
 	var svg = d3.select('#plot-area')
 		.append('svg')
-		.attr('width', width)
-		.attr('height', height)
+		.attr('width', width+margin.left+margin.right)
+		.attr('height', height+margin.top+margin.bottom)
 	var tooltip = d3.select("body").append("div")
 	    .attr("class", "tooltip")
 	    .style("opacity", 0);
-	function xValue(d){
+	function x(d){
 		return +d['case.fatality.rate']
 	}
 	function xMap(d){
@@ -45,8 +51,21 @@ function draw(data){
 		.range([height, height*.75, 0])
 	var fillScale = d3.scale.category10()
 		.domain(d3.set(data.map(fill)))
+	var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+	
+	svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate("+margin.left+"," + height + ")")
+        .call(xAxis)
+    .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("");
 
 	var points = svg.append('g')
+		.attr('transform', 'translate(+'+margin.left+','+margin.top+')')
 		.attr('class', 'points-group')
 		.selectAll('.point')
 		.data(data)
@@ -58,7 +77,11 @@ function draw(data){
 		.attr('cy', function(d){
 			return yScale(y(d))
 		})
-		.attr('fill', colour)
+		.attr('fill', "#fff")
+
+	points.transition()
+			.duration(500)
+			.attr('fill', colour)
 
 	points.on("mouseover", function(d) {
           tooltip.transition()

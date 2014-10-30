d439b1998ff0ec35bf231e10e2dca04ee1274ed7
Michael Malecki <mike@crunch.io>
2014-10-30 16:04:45 -0400

    demo 3

--- labs/D3-tutorial/germs.js
+++ labs/D3-tutorial/germs.js
@@ -1,16 +1,19 @@
+var data;
+
 window.onload = function(){
-	var hello = 'Hello world!'
-
-	d3.select('#plot-area')
-		.append('p')
-		.on('mouseover', applyHoverStyle)
-		.on('mouseout', removeHoverStyle)
-		.text(hello)
-
-	function applyHoverStyle(){
-		d3.select(this).classed({hovered: true})
-	}
-	function removeHoverStyle(){
-		d3.select(this).classed({hovered : false})
-	}
+	d3.json('germs.json', function(error, json){
+		data = json
+		draw(json)
+	})
+}
+function draw(data){
+	console.log(data)
+	var svg = d3.select('#plot-area')
+		.append('svg')
+	svg.selectAll('.point')
+		.data(data)
+		.enter()
+		.append('circle')
+		.classed('point', true)
+		.attr('cr', 2)
 }

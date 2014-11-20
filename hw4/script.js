08b19c8516aafa2ce94c43c53d9eb434180f53d5
frankchen07 <frankchen07@gmail.com>
2014-11-20 00:42:19 -0500

    stacked bar chart! stacked bar chart! needs comments and some debugging for the hoverovers

--- hw4/script.js
+++ hw4/script.js
@@ -1,73 +1,180 @@
-/* 
-steps for mapping our ecstasy project data:
-1. obtain data via R scraping and cleaning scripts
-2. save in CSV format
-3. ecstasy data, each "observation" (row) consists of the year, the category (pure MDMA, more MDMA, less MDMA, no MDMA, and unknown), count, and proportion by year).
-4. before we start, we have to reference some scripts in our index.html, specifically, d3.js, style.css, and this script, script.js. 
-5. I would like to make a stacked bar chart by year, and within each year, have stacked bars representing the four categories of pure MDMA, more MDMA, less MDMA, no MDMA, and unknown; for the sake of the assignment, let's first just feed in the "count" property of each object into a simple bar chart using SVGs. Once that is done, further updates to move towards the stacked bar chart will come!
-*/
+var margin = {top: 20, right: 55, bottom: 30, left: 40},
+    width = 1000 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-/*
-d3 steps:
-1. first decide on the size of a new SVG by defining variables width and height
-2. use chain methods to make a variable called svg (for faster indexing later on) that finds the body in the DOM, appends a yet to be created svg element, and gives it some attributes (e.g. width and height).
-3. using that defined variable svg, we generate some rectangles and give them some scalable, dynamic attributes so that they don't just lie on top of each other
-4. this is also the time where we ask for the proper data to be appended to the rectangles that are being created
-5. write some dynamic functions that deal with the upside-down issues of svg
-*/
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
 
-/*
-while writing the code below, I had the for loop that saves obj_count originally outside the callback function, which didn't work, as the entire script was running without my data actually loaded into "dataset." Jamming the code that extracts counts from each object in "dataset" into the callback function fixed the issue; our d3 code should also be within the callback function
-*/
+var y = d3.scale.linear()
+    .range([height, 0]);
 
-/*
-loading the data and getting it to proper numeric formats
-*/
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var area = d3.svg.area()
+  .interpolate("cardinal")
+  .x(function (d) { return x(d.label) + x.rangeBand() / 2; })
+  .y0(function (d) { return y(d.y0); })
+  .y1(function (d) { return y(d.y0 + d.y); });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var color = d3.scale.ordinal()
+  .range(["#001c9c","#101b4d","#475003","#9c8305","#d3c47c"]);
+
+var stack = d3.layout.stack()
+    .offset("zero")
+    .values(function (d) { return d.values; })
+    .x(function (d) { return x(d.label); })
+    .y(function (d) { return d.value; });
 
 var dataset;  //set global variable
-var obj_count = [];  //create an empty array for desired data later
 
-d3.csv("mdma_prop.csv", function(d) {  //d3.csv can take a callback function
+//insert variables 
+
+//loading data
+
+d3.csv("mdma_cast.csv", function(d) {  //d3.csv can take a callback function
   return {
-    count: +d.count,  //convert to numeric
-    mdma: d.mdma,
-    proportion: +d.proportion, //convert to numeric
-    year: new Date(+d.year, 0, 1)  //convert to date
+    year: d.year,
+    pure: +d.pure, //convert to numeric
+    more: +d.more,  //covert to numeric
+    less: +d.less, //convert to numeric
+    none: +d.none, //convert to numeric
+    unknown: +d.unknown  //convert to numeric;  date //new Date(+d.year, 0, 1)
   };
-}, function(error, data) {
+},
+
+function(error, data) {
   if (error) {
     return console.warn(error);
   } else {  //if error, warn us in console
     dataset = data;  //once loaded, store in variable dataset
     console.log(data);  //once loaded, log to console for proof
-    for(index in dataset) {  //log count values that we want to console
-      console.log(dataset[index].count); 
-    } 
-    for(index in dataset) {   
-      obj_count[index] = dataset[index].count;  //saves object count
-    }
-    }
-    var w = 1000;
-    var h = 200;
-    var barPadding = 2;
-    var svg = d3.select("body")  //selects body
-                .append("svg")  //appends empty svg element
-                .attr("width", w)   //assigns pre-defined width
-                .attr("height", h);  //assigns pre-defined height
-    svg.selectAll("rect")  //selects empty rectangles
-        .data(obj_count)  //uses our data
-        .enter()  //enters our data and hands it off for processing, each datum gets a rectangle
-        .append("rect")  //inserts corresponding rects into the DOM
-        .attr("x", function(d, i) {  //index * (1000/data length) = position
-          return i * (w / obj_count.length);
-        })
-        .attr("y", function(d) {  //this tells us where height begins, 0 means it starts at the top, h, or 200, is at the bottom, h - d tells it to begin part way down, and fill to the bottom
-          return h - d;
-        })
-        .attr("width", (w / obj_count.length) - barPadding)  //width = position minus bar padding
-        .attr("height", function(d) {  //height using data
-          return d
-        }); 
+  }
+  
+  var labelVar = "year";  //categorical variable that has labels for the chart
+  console.log(labelVar); 
+  var varNames = d3.keys(dataset[0])  //array of variable names for each series
+    .filter(function(key) { return key !== labelVar;});
+  console.log(varNames);
+
+  color.domain(varNames);
+
+  var seriesArr = [], series = {};  //basic structure of series array
+  varNames.forEach(function(name) {
+    series[name] = {name: name, values:[]};
+    seriesArr.push(series[name]);
+  });
+  console.log(seriesArr); 
+
+  dataset.forEach(function(d) {  //populates the previous array we made
+    varNames.map(function(name) {
+      series[name].values.push({label: d[labelVar], value: +d[name]});
+    });
+  });
+  console.log(seriesArr);
+
+  x.domain(dataset.map(function(d) { return d.year; })); //categorical variable year for our x-axis domain
+
+  stack(seriesArr);  //yo describe this shit
+  console.log("stacked seriesArr", seriesArr);
+
+  y.domain([0, d3.max(seriesArr, function(c) {  ///look through each series, each values, and establish min and max
+    return d3.max(c.values, function(d) { return d.y0 + d.y; });
+  })]);
+
+  svg.append("g")  //x-axis stuff
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+  
+  svg.append("g")  //y-axis stuff
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Composition Count");
+
+  var selection = svg.selectAll(".series")
+    .data(seriesArr)
+    .enter().append("g")
+    .attr("class", "series");
+
+  selection.append("path")
+    .attr("class", "streamPath")
+    .attr("d", function (d) { return area(d.values); })
+    .style("fill", function (d) { return color(d.name); })
+    .style("stroke", "grey");
+
+  /*var points = svg.selectAll(".seriesPoints")
+    .data(seriesArr)
+    .enter().append("g")
+    .attr("class", "seriesPoints");
+        
+  points.selectAll(".point")
+    .data(function (d) { return d.values; })
+    .enter().append("circle")
+    .attr("class", "point")
+    .attr("cx", function (d) { return x(d.label) + x.rangeBand() / 2; })
+    .attr("cy", function (d) { return y(d.y0 + d.y); })
+    .attr("r", "10px")
+    .style("fill",function (d) { return color(d.name); })
+    .on("mouseover", function (d) { showPopover.call(this, d); })
+    .on("mouseout",  function (d) { removePopovers(); })*/
+  
+  var legend = svg.selectAll(".legend")
+    .data(varNames.slice().reverse())
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function (d, i) { return "translate(55," + i * 20 + ")"; });
+  
+  legend.append("rect")
+    .attr("x", width - 10)
+    .attr("width", 10)
+    .attr("height", 10)
+    .style("fill", color)
+    .style("stroke", "grey");
+  
+  legend.append("text")
+    .attr("x", width - 12)
+    .attr("y", 6)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(function (d) { return d; });
+
+  /*function removePopovers () {
+    $('.popover').each(function() {
+      $(this).remove();
+    }); 
+  }
+  
+  function showPopover (d) {
+    $(this).popover({
+      title: d.name,
+      placement: 'auto top',
+      container: 'body',
+      trigger: 'manual',
+      html : true,
+      content: function() { 
+        return "Quarter: " + d.label + 
+               "<br/>Rounds: " + d3.format(",")(d.value ? d.value: d.y1 - d.y0); }
+    });
+    $(this).popover('show')
+  }*/
+
 });
 
 
